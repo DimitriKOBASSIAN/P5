fd9dfebd68403901063044ce8a0b5723e9b9bbc8
Dimitri Kobassian <d.kobassian@gmail.com>
2024-06-03 13:27:25 +0200

    Removed React Icons librairy, loading svg directly.

--- src/components/CollapseBar.jsx
+++ src/components/CollapseBar.jsx
@@ -1,38 +1,47 @@
 import { useState } from "react";
 import "../styles/CollapseBar.scss";
-import { FaChevronUp } from "react-icons/fa";
-import { FaChevronDown } from "react-icons/fa";
+import { ReactComponent as ChevronUp } from '../assets/icons/chevron-up-solid.svg';
+import { ReactComponent as ChevronDown } from '../assets/icons/chevron-down-solid.svg';
 
 function Collapsebar({ list }) {
     const [activeIndices, setActiveIndices] = useState([]);
 
     const handleClick = (index) => {
         if (activeIndices.includes(index)) {
-        setActiveIndices(activeIndices.filter((i) => i !== index));
+            setActiveIndices(activeIndices.filter((i) => i !== index));
         } else {
-        setActiveIndices([...activeIndices, index]);
+            setActiveIndices([...activeIndices, index]);
         }
     };
 
     return (
         <div className="collapse-bar">
-        {list.map((item, index) => (
-            <div key={index} className="collapse-bar__item">
-            <div
-                className="collapse-bar__item__header"
-                onClick={() => handleClick(index)}
-            >
-                <h2>{item.title}<span>{activeIndices.includes(index) ? <FaChevronDown /> : <FaChevronUp />}</span></h2>
-            </div>
-            {activeIndices.includes(index) && (
-                <div className="collapse-bar__item__content">
-                <p>{item.content}</p>
+            {list.map((item, index) => (
+                <div key={index} className="collapse-bar__item">
+                    <div
+                        className="collapse-bar__item__header"
+                        onClick={() => handleClick(index)}
+                    >
+                        <h2>
+                            {item.title}
+                            <span style={{ display: 'inline-block', width: '24px', height: '24px' }}>
+                                {activeIndices.includes(index) ? (
+                                    <ChevronDown className="chevron rotate" />
+                                ) : (
+                                    <ChevronUp className="chevron rotate" />
+                                )}
+                            </span>
+                        </h2>
+                    </div>
+                    {activeIndices.includes(index) && (
+                        <div className="collapse-bar__item__content">
+                            <p>{item.content}</p>
+                        </div>
+                    )}
                 </div>
-            )}
-            </div>
-        ))}
+            ))}
         </div>
     );
-    }
+}
 
 export default Collapsebar;
