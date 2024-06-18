02ceebb6c942ee89a1e5536f00bf360e5d93dd91
Dimitri Kobassian <d.kobassian@gmail.com>
2024-06-18 13:49:36 +0200

    Correction of a warning in Card.jsx and css update for the height in the content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,52 +3,44 @@ import { Link } from "react-router-dom";
 import data from "../data/logements.json";
 import "../styles/Card.scss";
 
-/* creating a function to display 6 card at loading. the usestate is initiated at 6, and we add 6 to it to allow display of more card if clicking on the button linked to showmorecards function */
 const Card = () => {
     const [numCardsToShow, setNumCardsToShow] = useState(6);
 
     const showMoreCards = () => {
-        setNumCardsToShow(numCardsToShow + 6);
+        setNumCardsToShow((prevNumCardsToShow) => prevNumCardsToShow + 6);
     };
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
-    
-
     return (
         <>
-        <section className="container">
-                <>
-                {data.slice(0, numCardsToShow).map((data) => ( 
-                    <figure key={data.id} className="container_card">
-                        <Link to={`/housing/${data.id}`} className="card">
+            <section className="container">
+                {data.slice(0, numCardsToShow).map((logement) => (
+                    <figure key={logement.id} className="container_card">
+                        <Link to={`/housing/${logement.id}`} className="card">
                             <img
-                                src={data.cover}
-                                alt={`nom de la location : ${data.title}`}
+                                src={logement.cover}
+                                alt={`nom de la location : ${logement.title}`}
                                 className="card__img"
                             />
                             <figcaption className="card__title">
-                                {data.title}
+                                {logement.title}
                             </figcaption>
                         </Link>
                     </figure>
                 ))}
-                </> 
-        </section>
-        <section >
-            {data.length > numCardsToShow && (
-                <div className="container_button">
-                    <button
-                        value="Afficher plus de contenu"
-                        onClick={showMoreCards}
-                    >
-                        Afficher plus
-                    </button>
-                </div>
-            )}
-        </section>
+            </section>
+            <section>
+                {data.length > numCardsToShow && (
+                    <div className="container_button">
+                        <button onClick={showMoreCards}>
+                            Afficher plus
+                        </button>
+                    </div>
+                )}
+            </section>
         </>
     );
 };
